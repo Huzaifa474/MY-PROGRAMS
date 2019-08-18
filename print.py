b67a131519d4a2e0f33a107050851e1f9b7240d2
Syed Muhommad Huzaifa Hssan <51955427+Huzaifa474@users.noreply.github.com>
2019-08-18 02:32:37 -0700

    Add files via upload

---
+++ print.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("HUZAIFA")#string data
+
+
+# In[5]:
+
+
+print("HUZAIFA","Rollno:93")#more than two values in single print statment 
+
+
+# In[6]:
+
+
+#BIO DATA
+print("Name:Syed Muhommad Huzaifa Hassan")
+print("Father Name:Syed Fareed ul Hassan")
+print("Nationality:Pakistani")
+print("Religion:Islam")
+print("Qualification:Undergraduate Student")
+
+
+# In[10]:
+
+
+print("Name:\t\tSyed Muhommad Huzaifa Hassan\n","Father Name:\tSyed Fareed ul Hassan\n","Nationality:\tPakistani\n","Religion:\tIslam\n","Qualification:\tUndergraduate Student\n")
+
+
+# In[1]:
+
+
+print(123,12.3)#integer,float
+
+
+# In[9]:
+
+
+print("HELLO","PYTHON",sep='*******')#with seprator
+
+
+# In[11]:
+
+
+print("Helloworld1")
+print("Hello world2")
+print("Hello world3")
+
+
+# In[12]:
+
+
+print("Helloworld1",end='')#end function work as a "\n"
+print("Hello world2")
+print("Hello world3")
+
+
+# In[13]:
+
+
+print("Helloworld1",end='\n\n')
+print("Hello world2",end='\n\n')
+print("Hello world3")
+
+
+# In[17]:
+
+
+print("\\Pakistan","\\i love Pakistan")#Backslash (\)
+
+
+# In[22]:
+
+
+print('\''"PAKISTAN"'\'')#SingleQuote
+
+
+# In[23]:
+
+
+print("\"""Pakistan Zindabad!""\"")#DoubleQuote
+
+
+# In[ ]:
+
+
+
+
