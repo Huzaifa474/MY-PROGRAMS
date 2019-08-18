7a7fdf9b277b175720309fb110cc2ce515168d42
Syed Muhommad Huzaifa Hssan <51955427+Huzaifa474@users.noreply.github.com>
2019-08-18 03:25:00 -0700

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#{'+','-','*','/'}
+print(24+24)
+
+
+# In[2]:
+
+
+#Addition
+a=24
+b=23
+c=a+b
+print(c)
+
+
+# In[3]:
+
+
+#Subraction
+a=20
+b=10.5
+c=b-a
+print("sum is",c)
+
+
+# In[4]:
+
+
+#Multiplication
+a=20
+b=10.5
+c=b*a
+print("multiplication is",c)
+
+
+# In[5]:
+
+
+a=20
+b=10
+c=a/b
+print("Division is",c)#Python by default floating point division
+#to get answer in integer weuse double // 
+d=a//b
+print("Divison is",d)#// discard  decimal points
+
+
+# In[6]:
+
+
+#'+' Operator in string work as concatination
+a="45"
+b="30"
+print(a+b)
+
+
+# In[7]:
+
+
+#HARDCODED VALUES CALCUALATOR
+a=2
+b=5
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+g=a%b
+print("2+5=",c)
+print("2-5=",d)
+print("2*5=",e)
+print("2/5=",f)
+print("2%5=",g)
+
+
+# In[ ]:
+
+
+
+
---
+++ Variables.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=10 #a is a varibale name which isalso called identifier
+# '=' is the assignment operator
+print(a)
+
+
+# In[2]:
+
+
+a=10.6#over riding of variables
+print(a)
+
+
+# In[8]:
+
+
+#to check the type of variable
+a=10.6
+type(a)
+
+
+# In[9]:
+
+
+a='HUZAIFA'
+type(a)
+
+
+# In[10]:
+
+
+a=10
+type(a)
+
+
+# In[14]:
+
+
+name='Huzaifa'
+city='Karachi'
+print(name,"lives in",city)
+
+
+# In[15]:
+
+
+#concatination
+a='Pakistan'
+b='zindabad'
+print(a+b)
+
+
+# In[16]:
+
+
+num1=3
+num2=5
+sum=num1+num2
+print(sum)
+
+
+# In[ ]:
+
+
+
+
