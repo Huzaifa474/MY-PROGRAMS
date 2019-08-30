d3874781ee6b287d97276eab42ab603066756151
Syed Muhommad Huzaifa Hssan <51955427+Huzaifa474@users.noreply.github.com>
2019-08-30 09:27:31 -0700

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,13 @@
+#tupple:it is another way to gather more than one value in one varibale same as list but the differenece is that in list we can change value but in tuple it remain constant technically it is calleed immetable
+atuple=[1,2,3,4,5,6,7,8,2]
+#to check length of tupple
+a=len(atuple)
+print("Lengt5h of tuple is:",a)
+#another way to create tupple
+b='Hello','python'
+print("Values of tupple are",b)
+#tupple has only two function
+#.count
+print(atuple.count(2))
+#.index
+print(atuple.index(3))
